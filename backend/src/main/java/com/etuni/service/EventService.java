b81eeff1c62766a06e4208870e3962f1145eadd5
Mehmet <mehmetkaplanceng@gmail.com>
2026-01-13 20:37:15 +0300

    fix: EventService transactional annotation added for lazy loading fix

--- backend/src/main/java/com/etuni/service/EventService.java
+++ backend/src/main/java/com/etuni/service/EventService.java
@@ -14,7 +14,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
+@Transactional
 public class EventService {
 
   private static final Logger log = LoggerFactory.getLogger(EventService.class);
